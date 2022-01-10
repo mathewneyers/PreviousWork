0e9cb1b14464202b94f6d05eae62c772394cda03
matneyers <neyers.mathew@student.greenriver.edu>
2022-01-10 14:34:02 -0800

    Created Hello Name Age

--- Python/helloworld.py
+++ Python/helloworld.py
@@ -3,5 +3,8 @@
 #This is a hello world script to
 #demonstrate python
 #Created by Mat Neyers on 01/10/22
-name = input("Enter Your Name:")
-print("Hello " + name)
+name = input("Enter your name:")
+currentYear = input("Enter the current year: ")
+yearBorn = input("Enter the year you were born: ")
+age = int(currentYear) - int(yearBorn)
+print("Hello " + name + ", you are " + str(age) + " years old.")
