0510bb75b0e82c47e12b79d4a6a15e0e1509e6f0
matneyers <neyers.mathew@student.greenriver.edu>
2022-02-02 11:03:21 -0800

    automate multiple devices

--- Python/GetUptime.py
+++ Python/GetUptime.py
@@ -2,11 +2,16 @@
 #Example pexpect on SSH
 import pexpect
 
-child = pexpect.spawn("ssh justincase@192.168.0.121")
-child.expect(".*password:")
-child.sendline("Password01")
-child.expect(".*\$")
-child.sendline("uptime")
-child.expect(".*\$")
-print(child.after)
-child.sendline("exit")
+def get_uptime(address):
+    child = pexpect.spawn("ssh justincase@" + address)
+    child.expect(".*password:")
+    child.sendline("Password01")
+    child.expect(".*\$")
+    child.sendline("uptime")
+    child.expect(".*\$")
+    print(child.after)
+    child.sendline("exit")
+
+ip_addresses = [ "192.168.0.111", "192.168.0.112", "192.168.0.121", "192.168.0.122"]
+for ip_address in ip_addresses:
+    get_uptime(ip_address)
